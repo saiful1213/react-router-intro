c9f0bb9f025e53ca2172341e4e9ea89d7add098b
saiful islam <saifulislamptcbd@gmail.com>
2024-10-30 12:46:48 -0700

    setup tailwind css, shadcn ui, react router in react project

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>hello form home</div>
+  },
+  {
+    path: '/about',
+    element: <div>hello form about</div>
+  },
+  {
+    path: '/contact',
+    element: <div>hello form contact</div>
+  }
+])
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
 )
